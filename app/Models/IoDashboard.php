e627cd0666f1a4afd1863b0610b35ec98640fd74
byu1009 <bayun1605@gmail.com>
2025-12-10 12:09:13 +0700

    perubahaniodashboard

--- app/Models/IoDashboard.php
+++ app/Models/IoDashboard.php
@@ -4,15 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class IoMappingDashboard extends Model
+class IoDashboard extends Model
 {
     public $timestamps = false;
-    protected $table = "io_mapping_dashboard";
+    protected $table = "io_dashboard";
     public $incrementing = true;
     protected $keyType = 'int';
     protected $primaryKey = 'dash_id';
     protected $fillable = [
-        'dash_nama',
+        'dash_name',
         'dash_status'
     ];
 }
